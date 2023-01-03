215660cf7a38b73db206d873b7b5632da4bbe8c1
Viper0Hub <120978351+Viper0Hub@users.noreply.github.com>
2023-01-03 07:10:34 +0100

    Add files via upload

---
+++ ViperHubLoad/PlaceIDList.lua
@@ -0,0 +1,8 @@
+getgenv().Games = {
+    [2768379856] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
+    [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
+    [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
+}
+
+getgenv().Unknown = {
+}
