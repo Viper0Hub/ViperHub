9913b1739743bd9d315e56b4fba2efb59b012ebf
Viper0Hub <120978351+Viper0Hub@users.noreply.github.com>
2023-01-05 01:12:35 +0100

    Update PlaceIDList.lua

--- ViperHubLoad/PlaceIDList.lua
+++ ViperHubLoad/PlaceIDList.lua
@@ -1,7 +1,7 @@
 getgenv().Games = {
-    [2768379856] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
-    [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
-    [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua" --SCP3008
+    [2768379856] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/Viper%20Hub%20Games/ViperHub_SCP.lua", --SCP3008
+    [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/Viper%20Hub%20Games/ViperHub_SCP.lua", --SCP3008
+    [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/Viper%20Hub%20Games/ViperHub_SCP.lua" --SCP3008
 }
 
 getgenv().Unknown = {
