9b9132ac187a30c947d6a3bf6fc85bc651f882ef
Viper0Hub <120978351+Viper0Hub@users.noreply.github.com>
2023-01-05 01:53:35 +0100

    Create ViperHub_NotSupported.lua

---
+++ Viper Hub Games/ViperHub_NotSupported.lua
@@ -0,0 +1,3 @@
+setclipboard("https://dsc.gg/argon-hub")
+wait(0.02)
+game.Players.LocalPlayer:kick("the game is not yet supported and is being programmed, join the discord server to be notified when it is released for this game, link has been copied to the clipboard")
