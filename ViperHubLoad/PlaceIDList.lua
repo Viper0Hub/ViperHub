33bbde92231fe8943d1132310ef44a8a07b21004
Viper0Hub <120978351+Viper0Hub@users.noreply.github.com>
2023-01-05 01:02:06 +0100

    Update PlaceIDList.lua

--- ViperHubLoad/PlaceIDList.lua
+++ ViperHubLoad/PlaceIDList.lua
@@ -1,7 +1,8 @@
 getgenv().Games = {
     [2768379856] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
     [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
-    [171391948] = "SOON", --Vehicle Simulator
+    [3824662112] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --SCP3008
+    [171391948] = "https://raw.githubusercontent.com/Viper0Hub/ViperHub/main/ViperHubGames/ViperHubSCP.lua", --Vehicle Simulator
 }
 
 getgenv().Unknown = {
